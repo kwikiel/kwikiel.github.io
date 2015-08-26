2b06165f046795e471e69b8239a4ac9bd89197fd
Kacper Wikieł <kwikiel@users.noreply.github.com>
2015-08-27 00:04:15 +0200

    Hackernews FTW

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,7 +26,7 @@ jQuery(document).ready(function($) {
     $("#rss-feeds").rss(
     
         //Change this to your own rss feeds
-        "http://feeds.feedburner.com/TechCrunch/startups",
+        "https://news.ycombinator.com/rss",
         
         {
         // how many entries do you want?
